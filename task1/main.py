dbae5fe882a8d64207f78161a1fd2ddfe7d0d2c9
ANTIN <antonzhelihovskiy@gmail.com>
2025-08-08 22:37:44 +0300

    Make task 1

---
+++ task1/main.py
@@ -0,0 +1,26 @@
+def caching_fibonacci():
+    # Створення порожнього словника
+    cache = {}
+    # Внутрішня функція
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n in cache:
+            return cache[n]
+        else:
+            cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+            return cache[n]
+    return fibonacci
+
+if __name__ == "__main__":
+    # Отримуємо функцію fibonacci
+    fib = caching_fibonacci()
+
+    # Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+    print(fib(10))
+    print(fib(15))
+
+
+
